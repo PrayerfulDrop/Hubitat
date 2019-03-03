3b3bb5761934434a638fcdd6aa76feb5152881d9
Aaron Ward <aaron@wardfamily.cc>
2019-03-03 09:47:27 -0600

    Update NOAA-Weather-Alerts.groovy

--- NOAA/beta/NOAA-Weather-Alerts.groovy
+++ NOAA/beta/NOAA-Weather-Alerts.groovy
@@ -10,7 +10,7 @@
  *
  *  Copyright 2018 Aaron Ward
  *
- *  Special thanks to bptorld, Cobra and csteele for use of their code or assistance in refining code.
+ *  Special thanks to csteele as he has been guiding me behind the scenes to make this project better and teaching me!
  *  
  *  This App is free and was designed for my needs originally but has grown for most needs too.
  *
@@ -28,7 +28,6 @@
  * ------------------------------------------------------------------------------------------------------------------------------
  *
  *  Changes:
- *   1.0.9 - fixed additional restriction testing, fixed PushOver Notification limitation of 1024 characters
  *   1.0.8 - fixed repeat in # minutes errors and execution
  *   1.0.7 - added ability to decide weather alert severity to check for
  *   1.0.6 - added testing option, repeat alert after # of minutes
@@ -41,10 +40,10 @@
 **/
 
 import groovy.json.*
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.regex.*
+
 	
-def version(){"v1.0.9"}
+def version(){"v1.0.8"}
 
 definition(
     name:"NOAA Weather Alerts",
@@ -151,9 +150,11 @@ def display(){
 	section() {
 		input "runTest", "bool", title: "Run a test Alert?", required: false, defaultValue: false, submitOnChange: true
 		if(runTest) {
-			testalert = "Testing Severe Weather Alert for the following counties: Springfield County.  The founder, Jebediah Springfield has spotted a cloud above the nuclear power plant towers.  Expect heavy polution, possible fish with three eyes, and a Simpson asleep at the console. . . This is the end of this Severe Weather Announcement."
-			talkNow(testalert)
 			runTest = false
+			testalert = "Testing Severe Weather Alert for the following counties: Springfield County.  The founder, Jebediah Springfield has spotted a cloud above the nuclear power plant towers.  Expect heavy polution, possible fish with three eyes, and a Simpson asleep at the console. . . This is the end of this Severe Weather Announcement."
+			//talkNow(testalert)
+			testalert = "We had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way— in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only.There were a king with a large jaw and a queen with a plain face, on the throne of England; there were a king with a large jaw and a queen with a fair face, on the throne of France. In both countries it was clearer than crystal to the lords of the State preserves of loaves and fishes, that things in general were settled for ever.It was the year of Our Lord one thousand seven hundred and seventy-five. Spiritual revelations were conceded to England at that favoured period, as at this. Mrs. Southcott had recently attained her five-and-twentieth blessed birthday, of whom a prophetic private in the Life Guards had heralded the sublime appearance by announcing that arrangements were made for the swallowing up of London and Westminster. Even the Cock-lane ghost had been laid only a round dozen of years, after rapping out its messages, as the spirits of this very year last past (supernaturally deficient in originality) rapped out theirs. Mere messages in the earthly order of events had lately come to the English Crown and People, from a congress of British subjects in America: which, strange to relate, have proved more important to the human race than any communications yet received through any of the chickens of the Cock-lane brood.France, less favoured on the whole as to matters spiritual than her sister of the shield and trident, rolled with exceeding smoothness down hill, making paper money and spending it. Under the guidance of her Christian pastors, she entertained herself, besides, with such humane achievements as sentencing a youth to have his hands cut off, his tongue torn out with pincers, and his body burned alive, because he had not kneeled down in the rain to do honour to a dirty procession of monks which passed within his view, at a distance of some fifty or sixty yards. It is likely enough that, rooted in the woods of France and Norway, there were growing trees, when that sufferer was put to death, already marked by the Woodman, Fate, to come down and be sawn into boards, to make a certain movable framework with a sack and a knife in it, terrible in history. It is likely enough that in the rough outhouses of some tillers of the heavy lands adjacent to Paris, there were sheltered from the weather that very day, rude carts, bespattered with rustic mire, snuffed about by pigs, and roosted in by poultry, which the Farmer, Death, had already set apart to be his tumbrils of the Revolution. But that Woodman and that Farmer, though they work unceasingly, work silently, and no one heard them as they went about with muffled tread: the rather, forasmuch as to entertain any suspicion that they were awake, was to be atheistical and traitorous.In England, there was scarcely an amount of order and protection to justify much national boasting. Daring burglaries by armed men, and highway robberies, took place in the capital itself every night; families were publicly cautioned not to go out of town without removing their furniture to upholsterers' warehouses for security; the highwayman in the dark was a City tradesman in the light, and, being recognised and challenged by his fellow-tradesman whom he stopped in his character of “the Captain,” gallantly shot him through the head and rode away; the mail was waylaid by seven robbers, and the guard shot three dead, and then got shot dead himself by the other four, “in consequence of the failure of his ammunition:” after which the mail was robbed in peace; that magnificent potentate, the Lord Mayor of London, was made to stand and deliver on Turnham Green, by one highwayman, who despoiled the illustrious creature in sight of all his retinue; prisoners in London gaols fought battles with their turnkeys, and the majesty of the law fired blunderbusses in among them, loaded with rounds of shot and ball; thieves snipped off diamond crosses from the necks of noble lords at Court drawing-rooms; musketeers went into St. Giles's, to search for contraband goods, and the mob fired on the musketeers, and the musketeers fired on the mob, and nobody thought any of these occurrences much out of the common way. In the midst of them, the hangman, ever busy and ever worse than useless, was in constant requisition; now, stringing up long rows of miscellaneous criminals; now, hanging a housebreaker on Saturday who had been taken on Tuesday; now, burning people in the hand at Newgate by the dozen, and now burning pamphlets at the door of Westminster Hall; to-day, taking the life of an atrocious murderer, and to-morrow of a wretched pilferer who had robbed a farmer's boy of sixpence.All these things, and a thousand like them, came to pass in and close upon the dear old year one thousand seven hundred and seventy-five. Environed by them, while the Woodman and the Farmer worked unheeded, those two of the large jaws, and those other two of the plain and the fair faces, trod with stir enough, and carried their divine rights with a high hand. Thus did the year one thousand seven hundred and seventy-five conduct their Greatnesses, and myriads of small creatures—the creatures of this chronicle among the rest—along the roads that lay before them."
+			pushNow(testalert)
 		}
  		input "logEnable", "bool", title: "Enable Debug Logging?", required: false, defaultValue: true
 		paragraph getFormat("line")
@@ -202,7 +203,7 @@ def refresh() {
 				if (logEnable) log.debug "AlertSent: '${alertsent}  Pastalert: '${state.pastalert}'"
 				if(alertsent != state.pastalert){
 					talkNow(state.alertmsg)
-					pushNow()
+					pushNow(state.alertmsg)
 					if(repeatYes) {
 						runIn((60*repeatMinutes.toInteger()),talkNow(state.alertmsg))
 					}
@@ -239,10 +240,10 @@ def talkNow(alertmsg) {
 		}
 }
 
-def pushNow() {
+def pushNow(alertmsg) {
 	if (pushovertts==true) {
-        Pattern p = Pattern.compile("\\b.{1,1023}\\b\\W?")
-        Matcher m = p.matcher(state.fullMsg1)
+        Pattern p = pattern.compile("(.|[\r\n]){1,1023}\\W")
+        Matcher m = p.matcher(alertmsg)
         while(m.find()) {
                 pushoverdevice.deviceNotification(m.group().trim())
         }
