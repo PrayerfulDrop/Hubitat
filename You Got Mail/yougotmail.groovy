f5a06cf85ba3d16e3bccbc594b440f8544aa2d1a
Aaron Ward <aaron@wardfamily.cc>
2019-09-02 11:23:02 -0500

    Update yougotmail.groovy

--- You Got Mail/yougotmail.groovy
+++ You Got Mail/yougotmail.groovy
@@ -107,7 +107,7 @@ def mainPage() {
             section(getFormat("header-green", " Mailbox Configuration")) {
                 input name: "maildeliveryStartTime", type: "time", title: "Mail Delivery Start Time:", required: true
         		input name: "maildeliveryStopTime", type: "time", title: "Mail Delivery End Time:", required: true
-                input name: "frequency", type: "text", title: "How many minutes to wait between notifications?", required: false, defaultValue: "1", submitOnChange: true
+                input name: "frequency", type: "text", title: "How many minutes to wait between notifications?", required: false, defaultValue: "5", submitOnChange: true
 		        input name: "mailboxcontact", type: "capability.contactSensor", title: "Choose contact sensor on mailbox:", required: true, multiple: false, submitOnChange: true
                 input name: "mailnotification", type: "text", title: "Message when mail is delivered: (for random messages seperate using semicolon)", required: false, submitOnChange: true, defaultValue: "Mail has arrived. You've. got. mail!;Look what was just delivered.  Your mail!;Tally ho!... Mail has arrived!"
                 paragraph "<b>Current message(s) to announce:</b><ul>"
