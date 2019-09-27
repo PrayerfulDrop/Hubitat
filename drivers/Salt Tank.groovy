83246d6634b3062584a915c465adc4f6184c6f31
Aaron Ward <aaron@wardfamily.cc>
2019-09-27 08:21:27 -0500

    Create Salt Tank.groovy

---
+++ drivers/Salt Tank.groovy
@@ -0,0 +1,146 @@
+/**
+ *  ****************  Salt Tank MQTT Driver  ****************
+ *
+ *  Design Usage:
+ *  This driver is designed to show dynamic icons of salt water tank and will connect to a MQTT broker.
+ *
+ *  Copyright 2019 Aaron Ward
+ *  
+ *  This driver is free and you may do as you like with it.  
+ *
+ * ------------------------------------------------------------------------------------------------------------------------------
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ * ------------------------------------------------------------------------------------------------------------------------------
+ *
+ *
+ *  Changes:
+ *
+ *  1.0.1 - added dynamic dashboard tile
+ *  1.0.0 - Modified Generic MQTT Driver
+ */
+
+metadata {
+    definition (name: "Salt Tank Driver", namespace: "aaronward", author: "Aaron Ward", importURL: "https://raw.githubusercontent.com/PrayerfulDrop/Hubitat/master/drivers/Salt%20Tank.groovy") {
+        capability "Initialize"
+        command "updateVersion"
+	command "publishMsg", ["String"]
+	attribute "delay", "number"
+	attribute "distance", "number"
+    attribute "saltlevel", "number"
+	attribute "dwDriverInfo", "string"
+    attribute "SaltTile", "string"
+	   }
+
+    preferences {
+        input name: "salttank", type: "text", title: "Softner tank Salt Block capacity:", required: true, displayDuringSetup: true, defaultValue: "4"
+        input name: "MQTTBroker", type: "text", title: "MQTT Broker Address:", required: true, displayDuringSetup: true
+		input name: "username", type: "text", title: "MQTT Username:", description: "(blank if none)", required: false, displayDuringSetup: true
+		input name: "password", type: "password", title: "MQTT Password:", description: "(blank if none)", required: false, displayDuringSetup: true
+		input name: "topicSub", type: "text", title: "Topic to Subscribe:", description: "Example Topic (topic/device/#)", required: false, displayDuringSetup: true
+		input name: "topicPub", type: "text", title: "Topic to Publish:", description: "Topic Value (topic/device/value)", required: false, displayDuringSetup: true
+        input name: "QOS", type: "text", title: "QOS Value:", required: false, defaultValue: "1", displayDuringSetup: true
+        input name: "retained", type: "bool", title: "Retain message:", required: false, defaultValue: false, displayDuringSetup: true
+	    input("logEnable", "bool", title: "Enable logging", required: true, defaultValue: true)
+    }
+
+}
+
+
+def installed() {
+    log.info "installed..."
+}
+
+def setVersion(){
+    appName = "SaltTankDriver"
+	version = "1.0.1" 
+    dwInfo = "${appName}:${version}"
+    sendEvent(name: "dwDriverInfo", value: dwInfo, displayed: true)
+}
+
+def updateVersion() {
+    log.info "In updateVersion"
+    setVersion()
+}
+
+// Parse incoming device messages to generate events
+def parse(String description) {
+	Date date = new Date(); 
+	topic = interfaces.mqtt.parseMessage(description).topic
+	topic = topic.substring(topic.lastIndexOf("/") + 1)
+	payload = interfaces.mqtt.parseMessage(description).payload
+	if (logEnable) log.debug topic
+	if (logEnable) log.debug payload
+	sendEvent(name: "${topic}", value: "${payload}", displayed: true)
+	sendEvent(name: "Last Payload Received", value: "Topic: ${topic} - ${date.toString()}", displayed: true)
+	state."${topic}" = "${payload}"
+	state.lastpayloadreceived = "Topic: ${topic} : ${payload} - ${date.toString()}"
+    tileNow()
+
+}
+
+def publishMsg(String s) {
+    if (logEnable) log.debug "Sent this: ${s} to ${settings?.topicPub} - QOS Value: ${settings?.QOS.toInteger()} - Retained: ${settings?.retained}"
+    interfaces.mqtt.publish(settings?.topicPub, s, settings?.QOS.toInteger(), settings?.retained)
+}
+
+def updated() {
+    if (logEnable) log.info "Updated..."
+    initialize()
+}
+
+def uninstalled() {
+    if (logEnable) log.info "Disconnecting from mqtt"
+    interfaces.mqtt.disconnect()
+}
+
+
+def initialize() {
+	if (logEnable) runIn(900,logsOff)
+    tileNow()
+	try {
+        if(settings?.retained==null) settings?.retained=false
+        if(settings?.QOS==null) setting?.QOS="1"
+        //open connection
+		mqttbroker = "tcp://" + settings?.MQTTBroker + ":1883"
+        interfaces.mqtt.connect(mqttbroker, "hubitat", settings?.username,settings?.password)
+        //give it a chance to start
+        pauseExecution(1000)
+        log.info "Connection established"
+		if (logEnable) log.debug "Subscribed to: ${settings?.topicSub}"
+        interfaces.mqtt.subscribe(settings?.topicSub)
+    } catch(e) {
+        if (logEnable) log.debug "Initialize error: ${e.message}"
+    }
+}
+
+def mqttClientStatus(String status){
+    if (logEnable) log.debug "MQTTStatus- error: ${status}"
+}
+
+def logsOff(){
+    log.warn "Debug logging disabled."
+    device.updateSetting("logEnable",[value:"false",type:"bool"])
+}
+
+def tileNow(){ 
+    if(salttank==null || salttank=="") salttank="4"
+    int saltlevel = (salttank.toInteger() * 12) - state.distance.toInteger()
+    if(saltlevel < 16 ) {img = "salt-low.png"
+                         msg="Low"}
+    if(saltlevel > 16 && salttank < (salttank.toInteger()/3*12*2)) {img = "salt-half.png"
+                                                                    msg="Half Full"}
+    if(saltlevel > (salttank.toInteger()/2*12)) {img = "salt-full.png"
+                                                 msg = "Full"}
+    sendEvent(name: "Salt Level", value: saltlevel, displayed: true)
+    img = "https://raw.githubusercontent.com/PrayerfulDrop/Hubitat/master/support/images/${img}"
+    tile = "<div style=font-size:15px align=center><img max-width=100% height=auto src=${img} border=0><br>Salt Level: ${msg}</div>"
+    sendEvent(name: "SaltTile", value: tile, displayed: true)
+}    
