c2d5fa6a541d329be8783c0c6e634b8168488fb0
Aaron Ward <aaron@wardfamily.cc>
2020-05-12 14:00:24 -0500

    Update NOAA Tile.groovy

--- NOAA/v3 beta/NOAA Tile.groovy
+++ NOAA/v3 beta/NOAA Tile.groovy
@@ -53,6 +53,7 @@ def logsOff(){
 
 def sendNoaaTile(noaaData) {
     if(logEnable) log.info "Received weather alert from NOAA App."
+    
     if(noaaData==null || noaaData=="") {
         noaaTile = "<center><table width='90%' height='90%'><tr>"
 		noaaTile += "<td style='text-align:center;'>"
@@ -65,7 +66,7 @@ def sendNoaaTile(noaaData) {
         fullalert = []
         log.error noaaData
         while(count) {
-            for(x=0;x<noaaData.alertmsg.size();x++) {
+            for(x=0;x<noaaData.size();x++) {
                 log.debug noaaData[x].alertmsg
 	    		def m = noaaData[x].alertmsg =~ /(.|[\r\n]){1,380}\W/
 		    	fullmsg = []
@@ -73,10 +74,11 @@ def sendNoaaTile(noaaData) {
     			   fullmsg << m.group()
                 }
                 for(i=0;i<fullmsg.size();i++) {
-                    noaaTile = "<center><table width='90%' height='90%'><tr>"
-				    noaaTile += "<td style='text-align: justify;'>"
-                    noaaTile += "<div style='font-size: 15px'> ${fullmsg[i]}</div>" 
-	    			noaaTile += "</td></tr></table>"
+                    noaaTile = "<center><table border=0 width='90%' height='90%'><thead><tr><th style='vertical-align:bottot;text-align: left;'><font style='font-size:15px;text-decoration:underline;'>"
+                    if(i==0) noaaTile += "Alert ${x}/${noaaData.size()}"
+                    else "Alert ${x}/${noaaData.size()} (continued)"
+				    noaaTile += "</font></th></tr></thead><tbody><tr><td style='text-align: justify;'><font style='font-size: 15px'> ${fullmsg[i]}</font>"
+	    			noaaTile += "</td></tr></tbody></table>"
 		    		sendEvent(name: "Alerts", value: noaaTile, displayed: true)                
                     pauseExecution(8000)
                 }
